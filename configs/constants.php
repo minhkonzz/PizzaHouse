87b17eb58766a3f445f7220a12dd5f26d3a03914
minhkonzz <minhphm37@gmail.com>
2023-05-22 17:23:05 +0700

    update 22-05-2023 05:23PM

--- configs/constants.php
+++ configs/constants.php
@@ -1,32 +1,27 @@
 <?php 
-  define("__ROOT__", $_SERVER["DOCUMENT_ROOT"] . "/pizza-complete-version/");
-  define("ROOT_CLIENT", "/pizza-complete-version/");
+
+  $is_local_env = $_SERVER["SERVER_NAME"] === "localhost";
+  define("__ROOT__", $_SERVER["DOCUMENT_ROOT"] . ($is_local_env ? "/pizza-complete-version/" : ""));
+  define("ROOT_CLIENT", $is_local_env ? "/pizza-complete-version/" : $_ENV["DOMAIN"]);
   define("__EXCEPTION__", "ExceptionController@handle");
   define("__CART_SESSION_KEY__", "cart");
   define("__CART_INITIAL__", [ "items" => [], "cart_total" => 0 ]);
   define("__SECRET_KEY_LENGTH__", 40);
   define("PAGE_TITLE_PREFIX", "Pizza House Việt Nam - ");
 
-  // define("__ROOT__", $_SERVER["DOCUMENT_ROOT"]);
-  // define("ROOT_CLIENT", $_ENV["DOMAIN"]);
-  // define("__EXCEPTION__", "ExceptionController@handle");
-  // define("__CART_SESSION_KEY__", "cart");
-  // define("__CART_INITIAL__", [ "items" => [], "cart_total" => 0 ]);
-  // define("__SECRET_KEY_LENGTH__", 40);
-
-  define("ROOT_ADMIN", $_SERVER["DOCUMENT_ROOT"] . "/pizza-complete-version/admin/");
-  define("ROOT_ADMIN_CLIENT", "/pizza-complete-version/admin/");
+  define("ROOT_ADMIN", $_SERVER["DOCUMENT_ROOT"] . ($is_local_env ? "/pizza-complete-version" : "" . "/admin/"));
+  define("ROOT_ADMIN_CLIENT", ($is_local_env ? "/pizza-complete-version" : $_ENV["DOMAIN"]) . "/admin/");
   define("OKTA_API_REQUEST_HEADERS", [
     "Accept: application/json", 
     "Content-Type: application/json", 
     "Authorization: SSWS " . $_ENV["OKTA_ACCESS_TOKEN"]
   ]);
 
-   // define("ROOT_ADMIN", $_SERVER["DOCUMENT_ROOT"] . "/admin/");
-   // define("ROOT_ADMIN_CLIENT", $_ENV["DOMAIN"] . "/admin/");
-   // define("OKTA_API_REQUEST_HEADERS", [
-   //    "Accept: application/json", 
-   //    "Content-Type: application/json", 
-   //    "Authorization: SSWS " . $_ENV["OKTA_ACCESS_TOKEN"]
-   // ]);
+  echo "__ROOT__: " . __ROOT__;
+  echo "<pre>"; 
+  echo "ROOT_CLIENT: " . ROOT_CLIENT;
+  echo "<pre>";  
+  echo "ROOT_ADMIN: " . ROOT_ADMIN;
+  echo "<pre>";
+  echo "ROOT_ADMIN_CLIENT: " . ROOT_ADMIN_CLIENT; 
 ?>
